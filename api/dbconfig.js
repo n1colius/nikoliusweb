e626521175199d241e91c2a47b40d88b47961181
Nikolius Lau <n1colius.lau@gmail.com>
2020-10-14 00:21:04 +0700

    update dbconfig

--- api/dbconfig.js
+++ api/dbconfig.js
@@ -1,37 +1,35 @@
-var mysql = require('mysql');
 const util = require('util')
+const mysql = require('mysql')
 
-var pool  = mysql.createPool({
-  connectionLimit : 10,
-  queueLimit: 100,
-  host : process.env.DBHOST,
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DBHOST,
   port : parseInt(process.env.DBPORT),
-  user : process.env.DBUSER,
-  password : process.env.DBPASS,
-  database : process.env.DBNAME,
-  connectTimeout : 10000,
-  waitForConnections: true,
-  acquireTimeout: 10000,
-  debug : false
-});
+  user: process.env.DBUSER,
+  password: process.env.DBPASS,
+  database: process.env.DBNAME
+})
 
-pool.on('connection', function (connection) {
-  console.log('MySQL DB Connection established');
-});
+// Ping database to check for common exception errors.
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection was closed.')
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('Database has too many connections.')
+    }
+    if (err.code === 'ECONNREFUSED') {
+      console.error('Database connection was refused.')
+    }
+  }
 
-pool.on('acquire', function (connection) {
-  console.log('Connection %d acquired', connection.threadId);
-});
+  if (connection) connection.release()
 
-pool.on('enqueue', function () {
-  console.log('Waiting for available connection slot...');
-});
-
-pool.on('release', function (connection) {
-  console.log('Connection %d released', connection.threadId);
-});
+  return
+})
 
 // Promisify for Node.js async/await.
 pool.query = util.promisify(pool.query)
 
-module.exports = pool;
+module.exports = pool
