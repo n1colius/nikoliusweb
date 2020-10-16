b7b156f1bcb8fc09bb98059f1d4d8d74854077b0
Nikolius Lau <n1colius.lau@gmail.com>
2020-10-16 23:24:14 +0700

    update soal redirect

--- api/index.js
+++ api/index.js
@@ -8,17 +8,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//redirect www ke non-www
-function redirectWwwTraffic(req, res, next) {
-	if (req.headers.host.slice(0, 4) === 'www.') {
-		var newHost = req.headers.host.slice(4);
-		return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
-	}
-	next();
-};  
-app.set('trust proxy', true);
-app.use(redirectWwwTraffic);
-
 // Require & Import API routes
 app.use('/authen', routes.authen);
 app.use('/blogman', routes.blogman);
--- server/index.js
+++ server/index.js
@@ -8,6 +8,17 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//redirect www ke non-www
+function redirectWwwTraffic(req, res, next) {
+	if (req.headers.host.slice(0, 4) === 'www.') {
+		var newHost = req.headers.host.slice(4);
+		return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+	}
+	next();
+};  
+app.set('trust proxy', true);
+app.use(redirectWwwTraffic);
+
 // Import and Set Nuxt.js options
 const config = require("../nuxt.config.js");
 config.dev = process.env.NODE_ENV !== "production";
