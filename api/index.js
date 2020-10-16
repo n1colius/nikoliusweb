f7792943759533f6e53bc83491e6980b0d97f6ab
Nikolius Lau <n1colius.lau@gmail.com>
2020-10-16 22:55:49 +0700

    tambahan body parser

--- api/index.js
+++ api/index.js
@@ -5,12 +5,17 @@ const bodyParser = require("body-parser");
 // Create express instnace
 const app = express();
 
+//Tambahan code dibawah ini belum coba di test di server, coba aja di server pakai npm run build dan jalankan pakai pm2
+//!!!!!!!!!!!!!!!
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Require & Import API routes
 app.use('/authen', routes.authen);
 app.use('/blogman', routes.blogman);
 
 // Export the server middleware
 module.exports = {
-  path: '/api',
-  handler: app
+	path: '/api',
+	handler: app
 };
